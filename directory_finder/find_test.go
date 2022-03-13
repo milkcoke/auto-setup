ba378e82c01a216bec853a415a2109f963083789
milkcoke <mbh023@gmail.com>
2022-03-13 22:47:58 +0900

    test: Add a test code for getting download directory from current user's home directory

---
+++ directory_finder/find_test.go
@@ -0,0 +1,26 @@
+package directory_finder
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+func TestFind(t *testing.T) {
+	downloadDirPath = GetDownloadDir()
+	if "C:\\Users\\CafeAlle\\Downloads" != downloadDirPath {
+		t.Error("Not Found Download directory from HOME directory")
+	} else {
+		file, err := os.Open(downloadDirPath)
+		if err != nil {
+			t.Error("Some error exists")
+		}
+
+		// Open download directory in explorer
+		cmd := exec.Command(`explorer`, filepath.Join(file.Name(), "./"))
+		fmt.Println(cmd)
+		cmd.Run()
+	}
+}
