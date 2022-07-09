28a2e452de0e4b864650a85c5b3cbd7c70b9440b
milkcoke <mbh023@gmail.com>
2022-07-09 13:37:50 +0900

    feat: Update the version of all app installers
    Rename field `uri` to `url` since link includes file extension.

--- install_manager/download.go
+++ install_manager/download.go
@@ -8,34 +8,29 @@ import (
 )
 
 type appInstallerInfo struct {
-	name      string
-	uri       string
-	extension string
+	name string
+	url  string
 }
 
 var apps = [...]appInstallerInfo{
 	{
 		name: "github_installer.exe",
-		uri:  "https://github.com/git-for-windows/git/releases/download/v2.35.1.windows.2/Git-2.35.1.2-64-bit.exe",
-		//extension: "exe",
+		url:  "https://github.com/git-for-windows/git/releases/download/v2.37.0.windows.1/Git-2.37.0-64-bit.exe",
 	}, {
 		name: "jetbrainToolBox_installer.exe",
-		uri:  "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.22.10970.exe",
-		//extension: "exe",
+		url:  "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.25.12424.exe",
 	}, {
 		name: "notion_installer.msi",
-		uri:  "https://www.notion.so/desktop/windows/download",
-		//extension: "msi",
+		url:  "https://www.notion.so/desktop/windows/download",
 	}, {
 		name: "nodejs_installer.msi",
-		uri:  "https://nodejs.org/dist/v16.14.0/node-v16.14.0-x64.msi",
-		//extension: "msi",
+		url:  "https://nodejs.org/dist/v16.16.0/node-v16.16.0-x64.msi",
 	}, {
 		name: "go-sdk-installer.msi",
-		uri:  "https://go.dev/dl/go1.18.windows-amd64.msi",
+		url:  "https://go.dev/dl/go1.18.3.windows-amd64.msi",
 	}, {
 		name: "powershell-installer.msi",
-		uri:  "https://github.com/PowerShell/PowerShell/releases/download/v7.2.2/PowerShell-7.2.2-win-x64.msi",
+		url:  "https://github.com/PowerShell/PowerShell/releases/download/v7.2.5/PowerShell-7.2.5-win-x64.msi",
 	}, {
 		name: "sourctree-installer.exe",
 		uri:  "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-3.4.8.exe",
@@ -53,7 +48,7 @@ func DownloadApps(downloadPath string) {
 			defer waitGroup.Done()
 
 			client := request.Client{
-				URL:    app.uri,
+				URL:    app.url,
 				Method: "GET",
 			}
 			resp := client.Send()
