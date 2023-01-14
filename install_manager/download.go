0f8e1d95caff9ccfa9d2487d23dcbfe8fb9d6dea
milkcoke <mbh023@gmail.com>
2023-01-14 14:17:02 +0900

    feat: Update installer version
    Update version of node.js, go-sdk, etc..

--- install_manager/download.go
+++ install_manager/download.go
@@ -15,28 +15,28 @@ type appInstallerInfo struct {
 var apps = [...]appInstallerInfo{
 	{
 		name: "github_installer.exe",
-		url:  "https://github.com/git-for-windows/git/releases/download/v2.37.1.windows.1/Git-2.37.1-64-bit.exe",
+		url:  "https://github.com/git-for-windows/git/releases/download/v2.39.0.windows.2/Git-2.39.0.2-64-bit.exe",
 	}, {
 		name: "jetbrainToolBox_installer.exe",
-		url:  "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.25.12627.exe",
+		url:  "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.27.2.13801.exe",
 	}, {
 		name: "notion_installer.exe",
 		url:  "https://www.notion.so/desktop/windows/download",
 	}, {
 		name: "nodejs_installer.msi",
-		url:  "https://nodejs.org/dist/v16.16.0/node-v16.16.0-x64.msi",
+		url:  "https://nodejs.org/dist/v18.13.0/node-v18.13.0-x64.msi",
 	}, {
 		name: "go-sdk-installer.msi",
-		url:  "https://go.dev/dl/go1.19.windows-amd64.msi",
+		url:  "https://go.dev/dl/go1.19.5.windows-amd64.msi",
 	}, {
 		name: "adobe-reader-installer.exe",
 		url:  "https://admdownload.adobe.com/rdcm/installers/live/readerdc64.exe",
 	}, {
 		name: "powershell-installer.msi",
-		url:  "https://github.com/PowerShell/PowerShell/releases/download/v7.2.5/PowerShell-7.2.5-win-x64.msi",
+		url:  "https://github.com/PowerShell/PowerShell/releases/download/v7.3.1/PowerShell-7.3.1-win-x64.msi",
 	}, {
 		name: "sourctree-installer.exe",
-		url:  "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-3.4.9.exe",
+		url:  "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-3.4.11.exe",
 	}, {
 		name: "docker-installer.exe",
 		url:  "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe",
